1dcdf25e5bb58545fbb63c2ef974d59d7e0145ff
aeroedgetechnologies <aeroedgetechnologies@gmail.com>
2025-06-27 08:07:35 -0700

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,6 +4,27 @@ import { sendOtp, verifyOtp } from '../controllers/authController.js';
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/auth/send-otp:
+ *   post:
+ *     summary: Send OTP to user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: OTP sent
+ *       400:
+ *         description: Bad request
+ */
 router.post('/send-otp', sendOtp);
 router.post('/verify-otp', verifyOtp);
 
