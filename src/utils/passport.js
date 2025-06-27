afaf59a83027bcd4f2c72850f9732431644d7dc0
aeroedgetechnologies <aeroedgetechnologies@gmail.com>
2025-06-27 08:06:37 -0700

    Update passport.js

--- src/utils/passport.js
+++ src/utils/passport.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import User from '../models/User.js';
@@ -7,7 +10,6 @@ passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: process.env.GOOGLE_CALLBACK_URL,
-
 }, async (accessToken, refreshToken, profile, done) => {
   let user = await User.findOne({ email: profile.emails[0].value });
   if (!user) {
@@ -24,5 +26,4 @@ passport.use(new GoogleStrategy({
   done(null, user);
 }));
 
-export default passport; 
-
+export default passport;
